7a893e8fe28b680872621841948112cdd391cdb4
Atef Awada <3atef3awada@gmail.com>
2026-05-16 03:41:39 +0200

    fixed api

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
-from python_practice.transaction_logic import load_transactions_csv
+from pydantic import BaseModel
+
+from python_practice.transaction_logic import load_transactions_json, save_transactions_json
 
 app = FastAPI()
 
-transactions = load_transactions_csv("./python_practice/transactions.csv")
+transactions = load_transactions_json("./python_practice/transactions.json")
+
+class Transaction(BaseModel):
+    amount: int
+    category: str
     
 @app.get("/total")
 def total():
@@ -22,3 +28,14 @@ def summary():
         result[cat] = result.get(cat, 0) + t["amount"]
         
     return result
+
+@app.post("/transactions")
+def add_transaction(transaction: Transaction):
+    transactions.append(transaction.model_dump())
+    
+    save_transactions_json(transactions, "./python_practice/transactions.json")
+    
+    return {
+        "status" : "added",
+        "transaction" : transaction
+    }
--- python_practice/transaction_logic.py
+++ python_practice/transaction_logic.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 def load_transactions_csv(filepath : str) -> list:
     with open(filepath, newline = '') as f:
@@ -8,3 +9,15 @@ def load_transactions_csv(filepath : str) -> list:
             row['amount'] = int(row['amount'])
             transactions_csv.append(row)
     return transactions_csv
+
+def load_transactions_json(filepath : str) -> list:
+    with open(filepath, "r") as f:
+        data = json.load(f)
+    return data
+        
+def save_transactions_json(
+    transactions : list,
+    filepath : str
+) -> None:
+    with open(filepath, "w") as f:
+        json.dump(transactions, f)
